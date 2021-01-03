58dad4a0aeb5a1e14c8f141b2743457be34d716b
Erison <erisonve@gmail.com>
2021-01-03 20:52:21 +0100

    Initial Commit

---
+++ APOD-VIPER/APOD_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  APOD_VIPERApp.swift
+//  APOD-VIPER
+//
+//  Created by Erison on 03/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct APOD_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
